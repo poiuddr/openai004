1694af5e9e6579ef9c0b1cc2916e7ed0cd70d909
poiuddr <ddr.ddr.poiu@gmail.com>
2024-09-05 12:37:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,71 @@
 import streamlit as st
-from PIL import Image
-import numpy as np
-import cv2
+import os
+os.environ["OPENAI_API_KEY"] = "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"
+import pandas as pd
+from llama_index.experimental.query_engine import PandasQueryEngine
 
-st.title("Image Converter")
-  
+USER_NAME = "USER"
+ASSISTANT_NAME = "AI"
+
+st.markdown('### 山田はなこ 田中あい 佐藤さき、について質問ができます。')
+st.markdown('質問の種類：年齢、バスト、3サイズ、プレー、コース、出勤中、出勤曜日、おすすめ、英語可能か、好きなプレイ、おすすめ')
+
+df = pd.DataFrame(
+  data = {
+      "girl name": ["山田はなこ", "田中あい", "佐藤さき"],
+      "age" :  [23, 21, 25],
+      "bust" :  ["80cm", "90cm", "100cm"],
+      "cup" :  ["C cup", "E cup", "H cup"],
+      "height": [160, 170, 180],
+      "hip": ["82cm", "88cm", "95cm"],
+      "waist": ["42cm", "48cm", "45cm"],
+      "available plays": [
+            ["anal", "blowjob", "vibration", "3p", "squirting"],
+            ["anal", "bondage", "vibration", "3p", "cumshot"],
+            ["blowjob", "bondage", "vibration", "3p", "titsfuck"],
+      ],
+      "currently working": ["no", "yes", "no"],
+      "english": ["yes", "no", "no"],
+      "course": [
+            ["VIP S", "VIP PREMIUM PERL", "IMAGE S"],
+            ["VIP PREMIUM RUBY", "IMAGE S"],
+            ["VIP S", "VIP PREMIUM SAPPHIRE"],
+      ],
+      "favorite play": ["3p", "bondage", "titsfuck"],
+      "work schedule": [
+          ["monday", "tuesday", "wednesday"],
+          ["thursday", "saturday", "sunday", "friday"],
+          ["wednesday", "thursday", "friday"],
+      ],
+      "recommended": ["yes", "no", "no"],
+      "shop": ["shinuku", "ikebukuro", "gotanda"]
+      }
+)
+
+query_engine = PandasQueryEngine(df=df, verbose=True, synthesize_response=True)
+
+if "chat_log" not in st.session_state:
+    st.session_state.chat_log = []
+
+# 質問の入力
+question = st.chat_input('質問を入力してください')
+
+if question:
+
+    for chat in st.session_state.chat_log:
+        with st.chat_message(chat["name"], avatar=chat["avatar"]):
+            st.write(chat["msg"])
+
+    response = query_engine.query(question)
+
+    with st.chat_message(USER_NAME, avatar="😃"):
+        st.write(question)
+
+    with st.chat_message(ASSISTANT_NAME, avatar="👩🏻"):
+        st.write(response.response)
+
+    #message = st.chat_message("assistant")
+    #message.write(response.response)
+
+    st.session_state.chat_log.append({"name": USER_NAME, "msg": question, "avatar": "😃"})
+    st.session_state.chat_log.append({"name": ASSISTANT_NAME, "msg": response.response, "avatar": "👩🏻‍💼"})
